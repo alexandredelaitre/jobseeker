dc29da44c0166212941ab1ab769ea0db94b616c4
alexandredelaitre <alex.delaitre123@gmail.com>
2022-02-19 14:25:57 +0000

    Added price test

--- jobseeker/mainSite/views.py
+++ jobseeker/mainSite/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 import json,requests
-
+import deso
 
 # Create your views here.
 from django.http import HttpResponse
 
-contractDecimals=18
+def current_price():
+    return deso.Deso().getDeSoPrice()
+
 
 def home(request):
-    return render(request,'home.html')
+    return render(request,'home.html',context=["price",current_price()])
