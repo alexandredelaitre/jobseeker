1a94e65eb53ca3b21c3b9be19fd6f72dc64abb61
rainestormee <ryanarrowsmith01@gmail.com>
2022-02-20 05:22:38 +0000

    create post

--- jobseeker/mainSite/views.py
+++ jobseeker/mainSite/views.py
@@ -1,31 +1,61 @@
 import deso
+import requests
 from django.shortcuts import render
+
 from .forms import JobForm
-from .models import Job
+
 
 # Create your views here.
 
 def current_price():
     return deso.Deso().getDeSoPrice()
 
-    
+
+def send_message(request):
+
+    publicKey = request.json['publicKey']
+    body = request.json['body']
+
+    header = {
+        "content-type": "application/json"
+    }
+
+    payload = {"UpdaterPublicKeyBase58Check": publicKey,
+               "PostHashHexToModify": "",
+               "ParentStakeID": "",
+               "Title": "",
+               "BodyObj": {"Body": body, "ImageURLs": []},
+               "RecloutedPostHashHex": "",
+               "PostExtraData": {},
+               "Sub": "",
+               "IsHidden": False,
+               "MinFeeRateNanosPerKB": 1000}
+
+
+    res = requests.post(
+        "https://bitclout.com/api/v0/submit-post", json=payload, headers=header)
+
+    resJson = res.json()
+    transactionHex = resJson["TransactionHex"]
+    print(transactionHex)
+
 
 def home(request):
-    if request.method=="POST":
-        form=JobForm()
+    if request.method == "POST":
+        form = JobForm()
         print(request.POST.keys())
         if form.is_valid():
             print("AAAA")
             form.save()
             print("BBBBB")
-            context={'form':form}
-        
+            context = {'form': form}
+
         print(request.POST['questName'])
-        questName=request.POST['questName']
-        summary=request.POST['questSummary']
-        questDesc=request.POST['questSummary']
-        questBounty=request.POST['questSummary']
-    context={}
+        questName = request.POST['questName']
+        summary = request.POST['questSummary']
+        questDesc = request.POST['questSummary']
+        questBounty = request.POST['questSummary']
+    context = {}
     quests = [
         {
             'title': 'Make hello world program',
@@ -34,9 +64,9 @@ def home(request):
             'user': 'arara',
             'bounty': str(5)
         }
-        
+
     ]
-    
+
     return render(request, 'home.html', context)
 
 # print(current_price()['price_24h'])
