d2c36063a0b7e667a3a36332e43ee90180296c7c
alexandredelaitre <alex.delaitre123@gmail.com>
2022-02-20 01:45:25 +0000

    Attempted to add a simple form

--- jobseeker/mainSite/models.py
+++ jobseeker/mainSite/models.py
@@ -1,2 +1,8 @@
+from telnetlib import Telnet
 from django.db import models
 
+class Job (models.Model):
+    questBounty=models.TextField(max_length=400)
+    questSummary=models.TextField(max_length=400)
+    questDesc=models.TextField(max_length=4000)
+    questName=models.TextField(max_length=50)
--- jobseeker/mainSite/views.py
+++ jobseeker/mainSite/views.py
@@ -1,14 +1,26 @@
 import deso
 from django.shortcuts import render
-
+from .forms import JobForm
+from .models import Job
 
 # Create your views here.
 
 def current_price():
     return deso.Deso().getDeSoPrice()
 
+    
 
 def home(request):
+    if request.method=="POST":
+        form=JobForm()
+        if form.is_valid():
+            print("AAAA")
+            form.save()
+            print("BBBBB")
+            context={'form':form}
+        
+        print(request.POST)
+    context={}
     quests = [
         {
             'title': 'Make hello world program',
@@ -17,7 +29,9 @@ def home(request):
             'user': 'arara',
             'bounty': str(5)
         }
+        
     ]
-    return render(request, 'home.html', context={'quests': quests})
+    
+    return render(request, 'home.html', context)
 
 # print(current_price()['price_24h'])
